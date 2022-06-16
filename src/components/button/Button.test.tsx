b49362fbcfd3e5131a140fd61b3263fc88b797b5
Marco Aurelio Moreira dos Anjos <marcoaure47@gmail.com>
2022-06-16 19:10:02 -0300

    test: add prop.text on Button

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -3,14 +3,14 @@ import Button from "./Button";
 
 
 describe("<Button />", () => {
-    it("should renders it", () => {
-        render(<Button />)
-        expect(screen.getByText(/hi/i)).toBeTruthy()
-    })
-
-    it("should be an html button", () => {
+    it("should be rendered and be an html button", () => {
         render(<Button />)
         expect(screen.getByRole("button")).toBeTruthy()
+    })
+
+    it("should my button accept prop name as button text", () => {
+        render(<Button text={"This is an Test"} />)
+        expect(screen.getByText(/This is an Test/i)).toBeTruthy()
     })
 })
 
