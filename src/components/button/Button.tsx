1c48d9894c32f843c08321c2af12dde62900d33e
Marco Aurelio Moreira dos Anjos <marcoaure47@gmail.com>
2022-06-16 19:56:18 -0300

    feat: add StyledButton

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,11 @@
 import { MouseEventHandler } from "react";
+import { StyledButton } from "./styles";
 
 interface ButtonProps {
     text?: string,
     callback?: MouseEventHandler
 }
 
-const Button = (props: ButtonProps) => <button onClick={props?.callback}>{props?.text}</button>;
+const Button = (props: ButtonProps) => <StyledButton onClick={props?.callback}>{props?.text}</StyledButton>;
 
 export default Button;
