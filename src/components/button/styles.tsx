8eece5cc0ea600525bc5aa79599a0323ef443579
Marco Aurelio Moreira dos Anjos <marcoaure47@gmail.com>
2022-06-17 10:20:39 -0300

    feat: add styles to button

--- src/components/button/styles.tsx
+++ src/components/button/styles.tsx
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export const StyledButton = styled.button``
+export const StyledButton = styled.button`
+    padding: 5px;
+    background: transparent;
+    text-transform: uppercase;
+    font-weight: bold;
+`
