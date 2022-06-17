ed0f6aa275740fdf15c99321c46089ee65b5e7bf
Marco Aurelio Moreira dos Anjos <marcoaure47@gmail.com>
2022-06-17 10:29:12 -0300

    test: add text props on colorLabel

--- src/components/colorLabel/ColorLabel.test.tsx
+++ src/components/colorLabel/ColorLabel.test.tsx
@@ -3,10 +3,17 @@ import ColorLabel from "./ColorLabel";
 
 
 describe("<ColorLabel />", () => {
-    it("should be rendered and be an div", () => {
+    it("should be rendered", () => {
         render(<ColorLabel />)
-        const searchForColorLabel = screen.getByTestId("color-label")
-        expect(searchForColorLabel).toBeTruthy()
+        const colorLabel = screen.getByTestId("color-label")
+        expect(colorLabel).toBeTruthy()
+    })
+
+    it("should have an text by props", () => {
+        render(<ColorLabel text={"this is an test"} />)
+        const colorLabel = screen.getByTestId("color-label")
+        
+        expect(colorLabel.textContent).toBe("this is an test")
     })
 })
 
